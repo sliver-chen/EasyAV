50edd15b6680707cb4ad61c7ee5abe7c234c4215
sliver <15505920258@163.com>
2018-11-22 21:32:39 +0800

    [feature]:base socket interface without event controller

--- include/EasyAV/Socket/TcpSocket.h
+++ include/EasyAV/Socket/TcpSocket.h
@@ -5,15 +5,21 @@
 #ifndef EASYAV_TCPSOCKET_H
 #define EASYAV_TCPSOCKET_H
 
+#include <stdio.h>
+
 namespace EasyAV {
 
     class TcpSocket {
     public:
-        TcpSocket() {}
+        TcpSocket();
 
-        virtual ~TcpSocket() {}
+        virtual ~TcpSocket();
+
+        ssize_t recv(void *buf, size_t len);
+
+        ssize_t send(void *buf, size_t len);
     private:
-
+        int mHandle;
     };
 
 }//namespace EasyAV
--- src/Socket/TcpSocket.cpp
+++ src/Socket/TcpSocket.cpp
@@ -3,7 +3,28 @@
 //
 
 #include "EasyAV/Socket/TcpSocket.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include "EasyAV/Util/EasyLog.h"
+
+#define MODULE_TAG "TcpSocket"
 
 namespace EasyAV {
+
+    TcpSocket::TcpSocket() : mHandle(0) {
+        mHandle = ::socket(AF_INET, SOCK_STREAM, 0);
+    }
+
+    TcpSocket::~TcpSocket() {
+        ::shutdown(mHandle, SHUT_RDWR);
+    }
+
+    ssize_t TcpSocket::recv(void *buf, size_t len) {
+        return ::recv(mHandle, buf, len, 0);
+    }
+
+    ssize_t TcpSocket::send(void *buf, size_t len) {
+        return ::send(mHandle, buf, len, 0);
+    }
 
 }//namespace EasyAV
