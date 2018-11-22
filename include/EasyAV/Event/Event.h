cec3391603d69994c1093a59ea80fd2942259773
sliver <15505920258@163.com>
2018-11-22 20:58:45 +0800

    [feature]:add event dir

---
+++ include/EasyAV/Event/Event.h
@@ -0,0 +1,12 @@
+//
+// Created by cx on 2018/11/22.
+//
+
+#ifndef EASYAV_EVENT_H
+#define EASYAV_EVENT_H
+
+namespace EasyAV {
+
+} //namespace EasyAV
+
+#endif //EASYAV_EVENT_H
---
+++ src/Event/Event.cpp
@@ -0,0 +1,9 @@
+//
+// Created by cx on 2018/11/22.
+//
+
+#include "EasyAV/Event/Event.h"
+
+namespace EasyAV {
+    
+} //namespace EasyAV
