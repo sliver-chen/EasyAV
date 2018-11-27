e599d1f344ea491d5f0d0cbe670983b19039735a
sliver <15505920258@163.com>
2018-11-28 00:28:25 +0800

    [feature]:add event interface(without runloop implementation)

--- include/EasyAV/Event/Event.h
+++ include/EasyAV/Event/Event.h
@@ -5,7 +5,50 @@
 #ifndef EASYAV_EVENT_H
 #define EASYAV_EVENT_H
 
+#include <sys/select.h>
+#include <vector>
+#include <thread>
+#include <functional>
+#include <unordered_map>
+
 namespace EasyAV {
+
+    typedef std::function<void(int event)> EventCB;
+
+    enum EventType {
+        EventRead  = 1 << 0,
+        EventWrite = 1 << 1,
+        EventError = 1 << 2,
+    };
+
+    typedef struct EventItem_{
+        int fd;
+        EventType type;
+        EventCB cb;
+    }EventItem;
+
+    class Event {
+    public:
+        Event();
+
+        virtual ~Event();
+
+        static Event *instance();
+
+        bool addEvent(int fd, EventType type, EventCB);
+
+        bool delEvent(int fd);
+
+        bool runEventLoop(bool isBlocked = false);
+    private:
+        fd_set *mSetRead;
+        fd_set *mSetWrite;
+        fd_set *mSetError;
+
+        std::vector<int> mSetVectors;
+        std::thread *mEventLoopThread;
+        std::unordered_map<int, EventItem> mEventMaps;
+    };
 
 } //namespace EasyAV
 
--- src/Event/Event.cpp
+++ src/Event/Event.cpp
@@ -3,7 +3,41 @@
 //
 
 #include "EasyAV/Event/Event.h"
+#include "EasyAV/Util/EasyLog.h"
+
+#define MODULE_TAG "Event"
 
 namespace EasyAV {
-    
+    Event::Event() {
+
+    }
+
+    Event::~Event() {
+
+    }
+
+    Event* Event::instance() {
+        static Event *context(new Event);
+        return context;
+    }
+
+    bool Event::addEvent(int fd, EasyAV::EventType type, EventCB cb) {
+        EventItem item;
+        item.fd = fd;
+        item.type = type;
+        item.cb = cb;
+        mEventMaps.emplace(fd, item);
+    }
+
+    bool Event::delEvent(int fd) {
+        mEventMaps.erase(fd);
+    }
+
+    bool Event::runEventLoop(bool isBlocked) {
+        if (isBlocked) {
+
+        } else {
+            mEventLoopThread = new std::thread(&Event::runEventLoop, this, true);
+        }
+    }
 } //namespace EasyAV
